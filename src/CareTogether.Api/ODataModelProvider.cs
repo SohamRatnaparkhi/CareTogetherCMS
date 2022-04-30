e8438d636d22eac7862c3073ca8eaf76a748bde3
Lars Kemmann <lars.kemmann@bynalogic.com>
2022-04-30 14:54:32 -0400

    OData model provider class

---
+++ src/CareTogether.Api/ODataModelProvider.cs
@@ -0,0 +1,16 @@
+﻿using CareTogether.Managers;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace CareTogether.Api
+{
+    public class ODataModelProvider
+    {
+        public static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<CombinedFamilyInfo>(nameof(CombinedFamilyInfo));
+            return builder.GetEdmModel();
+        }
+    }
+}
